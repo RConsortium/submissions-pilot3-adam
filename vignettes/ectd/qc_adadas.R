4a553ea080a66e07bf835c1978a71a74ee898fdb
Kangjie Zhang <kangjie.zhang@bayer.com>
2022-11-29 18:44:34 +0000

    init qc_adadas.R

---
+++ vignettes/ectd/qc_adadas.R
@@ -0,0 +1,17 @@
+###########################################################################
+#' developers : Kangjie Zhang/
+#' date: 29NOV2022
+#' modification History:
+#' QC ADADAS
+###########################################################################
+
+# Set up ------------------------------------------------------------------
+library(haven)
+library(diffdf)
+library(dplyr)
+
+# read source -------------------------------------------------------------
+#qc_adadas <- haven::read_xpt(file.path("adam", "adadas.xpt"))
+
+# compare R generated adadas with adam/adadas -----------------------------
+#diffdf(adadas , qc_adadas , suppress_warnings = T)
