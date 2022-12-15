e3ed5396596ca1e72a3dc6da9d096b6b48d34255
Declan Hodges <declan.x.hodges@gsk.com>
2022-12-15 12:53:39 +0000

    Export functions in pilot3.

--- R/adam_functions.R
+++ R/adam_functions.R
@@ -1,6 +1,8 @@
 #' Derive pooled site group
 #'
 #' `format_sitegr1` derives the `SITEGR1` variable, which pools sites with fewer than 3 subjects in any one treatment group.
+#'
+#' @export
 format_sitegr1 <- function(x) {
   case_when(
     x %in% c("702", "706", "707", "711", "714", "715", "717") ~ "900",
@@ -11,6 +13,8 @@ format_sitegr1 <- function(x) {
 #' Derive pooled age group
 #'
 #' `format_agegr1` derives the `AGEGR1` variable, which pools subjects into age groups.
+#'
+#' @export
 format_agegr1 <- function(x) {
   case_when(
     x < 65 ~ "<65",
@@ -22,6 +26,8 @@ format_agegr1 <- function(x) {
 #' Derive pooled age group (N)
 #'
 #' `format_agegr1n` derives the `AGEGR1N` variable, which is the numeric code for `AGEGR1`.
+#'
+#' @export
 format_agegr1n <- function(x) {
   case_when(
     x < 65 ~ 1,
@@ -33,6 +39,8 @@ format_agegr1n <- function(x) {
 #' Derive race (N)
 #'
 #' `format_racen` derives the `RACEN` variable, which is the numeric code for `RACE`.
+#'
+#' @export
 format_racen <- function(x) {
   case_when(
     x == "WHITE" ~ 1,
@@ -44,6 +52,8 @@ format_racen <- function(x) {
 #' Derive pooled baseline BMI group
 #'
 #' `format_bmiblgr1` derives the `BMIBLGR1` variable, which pools subjects into baseline BMI groups.
+#'
+#' @export
 format_bmiblgr1 <- function(x) {
   case_when(
     !is.na(x) & x < 25 ~ "<25",
@@ -55,6 +65,8 @@ format_bmiblgr1 <- function(x) {
 #' Derive pooled disease duration group
 #'
 #' `format_durdsgr1` derives the `DURDSGR1` variable, which pools subjects into disease duration groups.
+#'
+#' @export
 format_durdsgr1 <- function(x) {
   case_when(
     !is.na(x) & x < 12 ~ "<12",
@@ -65,6 +77,8 @@ format_durdsgr1 <- function(x) {
 #' Derive subject's disposition reason(s)
 #'
 #' `format_dcsreas` derives the `DCSREAS` variable, which contains subjects' disposition reason(s).
+#'
+#' @export
 format_dcsreas <- function(dsdecod) {
   case_when(
     dsdecod == "ADVERSE EVENT" ~ "Adverse Event",
