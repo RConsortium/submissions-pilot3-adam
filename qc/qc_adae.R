11ebbb2261ab8ff19284a608b8f5c521cb785444
Joel Laxamana <joel.laxamana@gmail.com>
2023-02-08 23:13:30 +0000

    updated to qc ADAE

--- qc/qc_adae.R
+++ qc/qc_adae.R
@@ -1,29 +1,19 @@
 ###########################################################################
 #' developers : Thomas Neitmann/
 #' date: 16DEC2022
+#' QC ADSL
 #' modification History:
-#' QC ADAE
+#'  laxamanaj 08FEB2023 : updated to QC ADAE
 ###########################################################################
 library(haven)
 library(diffdf)
 
-adsl <- read_xpt(file.path("submission", "datasets", "adsl.xpt"))
-qc_adsl <- read_xpt(file.path("adam", "adsl.xpt"))
-
-diffdf(adsl, qc_adsl, keys = c("STUDYID", "USUBJID"))
-
-
 # -------------------------------------------#
 # QC/Check against original TDF ADAE dataset #
 # -------------------------------------------#
-# adsl  <- haven::read_xpt("https://github.com/RConsortium/submissions-pilot3-adam/blob/main/adam/adsl.xpt?raw=true")
-# adae_orig <- haven::read_xpt("https://github.com/RConsortium/submissions-pilot3-adam/blob/main/adam/adae.xpt?raw=true") %>% 
-#   convert_blanks_to_na() #blanks to NA
-
-
 adae <- read_xpt(file.path("submission", "datasets", "adae.xpt"))
-qc_adsl <- read_xpt(file.path("adam", "adae.xpt")) %>% 
-  convert_blanks_to_na() #blanks to NA
+adae_orig <- read_xpt(file.path("adam", "adae.xpt"))
+
 #---------#
 # Compare #
 #---------#
