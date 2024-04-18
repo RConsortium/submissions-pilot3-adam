6efc1291c141950259fc381c70f0cd98da827705
Joel Laxamana <joel.laxamana@gmail.com>
2024-04-18 17:07:58 +0000

    updated footnotes

--- submission/programs/tlf-efficacy.r
+++ submission/programs/tlf-efficacy.r
@@ -146,16 +146,9 @@ tbl_1 <- apr0ancova1 %>%
       "{^b} Based on an Analysis of covariance (ANCOVA) model with treatment and baseline value as covariates",
       "CI = Confidence Interval, LS = Least Squares, SD = Standard Deviation"
     )
-  ) %>%
-  rtf_footnote(
-    footnote = c(
-      paste("Table generated on:", Sys.time())
-    ),
-    as_table = FALSE,
-    text_justification = "c"
-  ) %>%
+  ) %>% 
   rtf_source(
-    source = "Source: [pilot3: adam-adsl; adlbc]",
+    source = c(paste("Table generated on:", Sys.time()),"Source: [pilot3: adam-adsl; adlbc]"),
     text_justification = "c"
   )
 
